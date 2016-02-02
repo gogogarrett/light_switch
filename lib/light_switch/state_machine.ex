def869d00c0fb54e81ef36edb29e4e3f392b0da5
Garrett Heinlen <heinleng@gmail.com>
2016-02-02 16:37:35 +1100

    adding an action to flip_switch_up (triggering the state change to `on`!)

--- lib/light_switch/state_machine.ex
+++ lib/light_switch/state_machine.ex
@@ -6,13 +6,31 @@ defmodule LightSwitch.StateMachine do
   end
 
   def get_state, do: :gen_fsm.sync_send_event(@name, :get_state)
+  def flip_switch_up, do: :gen_fsm.send_event(@name, :flip_switch_up)
 
   # GenFSM api
   def init(state) do
     {:ok, :off, state}
   end
 
+  @doc """
+  Sync call that returns the current state of the state machine
+  """
   def off(:get_state, _from, state) do
     {:reply, :off, :off, state}
+  end
+
+  @doc """
+  Async event that flips the switch up.. which as a result changes the state to `on`
+  """
+  def off(:flip_switch_up, state) do
+    {:next_state, :on, state}
+  end
+
+  @doc """
+  Sync call that returns the current state of the state machine
+  """
+  def on(:get_state, _from, state) do
+    {:reply, :on, :on, state}
   end
 end
--- test/light_switch/state_machine_test.exs
+++ test/light_switch/state_machine_test.exs
@@ -9,4 +9,9 @@ defmodule LightSwitch.StateMachineTest do
   test "get_state" do
     assert LightSwitch.StateMachine.get_state == :off
   end
+
+  test "flip_switch_up switches light on!" do
+    LightSwitch.StateMachine.flip_switch_up
+    assert LightSwitch.StateMachine.get_state == :on
+  end
 end

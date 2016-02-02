976603b48a93538e10afd0621dd76eb4a97767c5
Garrett Heinlen <heinleng@gmail.com>
2016-02-02 17:26:23 +1100

    Creating a test to flip the switch down to toggle the state back to off
    Required a bit of hacking to ensure the state is always in `off` before
    the next test is run. I would really appreciate some input on how to do
    this better.

--- lib/light_switch/state_machine.ex
+++ lib/light_switch/state_machine.ex
@@ -7,6 +7,7 @@ defmodule LightSwitch.StateMachine do
 
   def get_state, do: :gen_fsm.sync_send_event(@name, :get_state)
   def flip_switch_up, do: :gen_fsm.send_event(@name, :flip_switch_up)
+  def flip_switch_down, do: :gen_fsm.send_event(@name, :flip_switch_down)
 
   # GenFSM api
   def init(state) do
@@ -32,5 +33,12 @@ defmodule LightSwitch.StateMachine do
   """
   def on(:get_state, _from, state) do
     {:reply, :on, :on, state}
+  end
+
+  @doc """
+  Async event that flips the switch down.. which as a result changes the state to `off`
+  """
+  def on(:flip_switch_down, state) do
+    {:next_state, :off, state}
   end
 end
--- test/light_switch/state_machine_test.exs
+++ test/light_switch/state_machine_test.exs
@@ -1,6 +1,15 @@
 defmodule LightSwitch.StateMachineTest do
   use ExUnit.Case
 
+  setup do
+    on_exit fn ->
+      # Ensure we're always in "off" state at the beginning of test
+      if LightSwitch.StateMachine.get_state == :on do
+        LightSwitch.StateMachine.flip_switch_down
+      end
+    end
+  end
+
   test "starts up" do
     pid = Process.whereis(:LSFSM)
     assert Process.alive?(pid) == true
@@ -13,5 +22,13 @@ defmodule LightSwitch.StateMachineTest do
   test "flip_switch_up switches light on!" do
     LightSwitch.StateMachine.flip_switch_up
     assert LightSwitch.StateMachine.get_state == :on
+  end
+
+  test "flip_switch_down switches light off" do
+    LightSwitch.StateMachine.flip_switch_up
+    assert LightSwitch.StateMachine.get_state == :on
+
+    LightSwitch.StateMachine.flip_switch_down
+    assert LightSwitch.StateMachine.get_state == :off
   end
 end
